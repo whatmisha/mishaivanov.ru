6307010807bf892b80bbbf33ee98ce9e1342fdf4
Misha Ivanov <chesta33@gmail.com>
2023-12-18 14:53:40 +0300

    Update sketch.js

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,5 +1,7 @@
 let x, y;
+let diameter = 30; // Начальный диаметр круга
 const step = 2;
+const sizeChangeStep = 1; // Шаг изменения размера
 let movingLeft = false;
 let movingRight = false;
 let movingUp = false;
@@ -30,31 +32,7 @@ function draw() {
     }
 
     fill(255); // Белый цвет круга
-    ellipse(x, y, 30, 30);
-}
-
-function keyPressed() {
-    if (keyCode === LEFT_ARROW) {
-        movingLeft = true;
-    } else if (keyCode === RIGHT_ARROW) {
-        movingRight = true;
-    } else if (keyCode === UP_ARROW) {
-        movingUp = true;
-    } else if (keyCode === DOWN_ARROW) {
-        movingDown = true;
-    }
-}
-
-function keyReleased() {
-    if (keyCode === LEFT_ARROW) {
-        movingLeft = false;
-    } else if (keyCode === RIGHT_ARROW) {
-        movingRight = false;
-    } else if (keyCode === UP_ARROW) {
-        movingUp = false;
-    } else if (keyCode === DOWN_ARROW) {
-        movingDown = false;
-    }
+    ellipse(x, y, diameter, diameter);
 }
 
 function handleGamepad() {
@@ -66,11 +44,23 @@ function handleGamepad() {
         let leftStickX = gp.axes[0];
         let leftStickY = gp.axes[1];
 
+        // Кнопки L2 и R2
+        let L2 = gp.buttons[6].value; // Значение от 0 до 1
+        let R2 = gp.buttons[7].value; // Значение от 0 до 1
+
         if (Math.abs(leftStickX) > 0.1) {
             x += leftStickX * step;
         }
         if (Math.abs(leftStickY) > 0.1) {
             y += leftStickY * step;
+        }
+
+        // Изменение размера круга
+        if (L2 > 0.1) {
+            diameter = max(10, diameter - L2 * sizeChangeStep); // Не уменьшать диаметр меньше 10
+        }
+        if (R2 > 0.1) {
+            diameter = min(100, diameter + R2 * sizeChangeStep); // Не увеличивать диаметр больше 100
         }
     }
 }
