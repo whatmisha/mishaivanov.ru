3bd0caaef03c07c04e4d7c3df2a1ad193515b3af
Misha Ivanov <chesta33@gmail.com>
2023-12-18 13:20:32 +0300

    p5 test

---
+++ p5/sketch.js
@@ -0,0 +1,26 @@
+let x, y;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    x = width / 2;
+    y = height / 2;
+}
+
+function draw() {
+    background(220);
+    fill(255, 0, 0);
+    ellipse(x, y, 30, 30);
+}
+
+function keyPressed() {
+    const step = 5;
+    if (keyCode === LEFT_ARROW) {
+        x -= step;
+    } else if (keyCode === RIGHT_ARROW) {
+        x += step;
+    } else if (keyCode === UP_ARROW) {
+        y -= step;
+    } else if (keyCode === DOWN_ARROW) {
+        y += step;
+    }
+}
