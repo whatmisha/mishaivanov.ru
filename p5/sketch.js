858d64800e6d72ed42239e5d7a8095577d1a93a9
Misha Ivanov <chesta33@gmail.com>
2023-12-18 14:30:15 +0300

    Update sketch.js

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,60 +1,24 @@
 let x, y;
-const step = 2; // Меньшее значение для более плавного управления
-let movingLeft = false;
-let movingRight = false;
-let movingUp = false;
-let movingDown = false;
+const step = 2;
+let drawing = false;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     x = width / 2;
     y = height / 2;
+    background(0); // Черный фон
 }
 
 function draw() {
-    background(0);
-
     handleGamepad();
 
-    if (movingLeft) {
-        x -= step;
-    }
-    if (movingRight) {
-        x += step;
-    }
-    if (movingUp) {
-        y -= step;
-    }
-    if (movingDown) {
-        y += step;
+    if (drawing) {
+        fill(255); // Белый цвет для рисования
+        ellipse(x, y, 30, 30);
     }
 
-    fill(255, 255, 255);
+    fill(255); // Белый цвет основного круга
     ellipse(x, y, 30, 30);
-}
-
-function keyPressed() {
-    if (keyCode === LEFT_ARROW) {
-        movingLeft = true;
-    } else if (keyCode === RIGHT_ARROW) {
-        movingRight = true;
-    } else if (keyCode === UP_ARROW) {
-        movingUp = true;
-    } else if (keyCode === DOWN_ARROW) {
-        movingDown = true;
-    }
-}
-
-function keyReleased() {
-    if (keyCode === LEFT_ARROW) {
-        movingLeft = false;
-    } else if (keyCode === RIGHT_ARROW) {
-        movingRight = false;
-    } else if (keyCode === UP_ARROW) {
-        movingUp = false;
-    } else if (keyCode === DOWN_ARROW) {
-        movingDown = false;
-    }
 }
 
 function handleGamepad() {
@@ -65,6 +29,7 @@ function handleGamepad() {
         // Левый стик контроллера
         let leftStickX = gp.axes[0];
         let leftStickY = gp.axes[1];
+        let stickPressed = gp.buttons[10].pressed; // L3 кнопка (нажатие левого стика)
 
         if (Math.abs(leftStickX) > 0.1) {
             x += leftStickX * step;
@@ -72,5 +37,7 @@ function handleGamepad() {
         if (Math.abs(leftStickY) > 0.1) {
             y += leftStickY * step;
         }
+
+        drawing = stickPressed;
     }
 }
