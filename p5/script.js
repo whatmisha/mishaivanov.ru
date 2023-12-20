7ec153f19eeed7c9aaa4088a02faf2f5eb7df516
Misha Ivanov <chesta33@gmail.com>
2023-12-20 18:35:47 +0300

    Index for p5 projects

---
+++ p5/script.js
@@ -0,0 +1,27 @@
+const repoOwner = 'имя_пользователя'; // Замените на ваше имя пользователя на GitHub
+const repoName = 'имя_репозитория';  // Замените на название вашего репозитория
+const path = 'p5'; // Путь к папке в вашем репозитории
+
+fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${path}`)
+    .then(response => response.json())
+    .then(data => {
+        const projectList = document.getElementById('project-list');
+        projectList.innerHTML = '';
+
+        data.forEach(item => {
+            if (item.type === 'dir') {
+                const link = document.createElement('a');
+                link.href = item.html_url;
+                link.textContent = item.name;
+                link.target = '_blank'; // Открывать в новой вкладке
+
+                const listItem = document.createElement('div');
+                listItem.appendChild(link);
+                projectList.appendChild(listItem);
+            }
+        });
+    })
+    .catch(error => {
+        console.error('Ошибка при получении данных с GitHub:', error);
+        document.getElementById('project-list').textContent = 'Ошибка загрузки проектов.';
+    });
