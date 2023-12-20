eb9b9d4609fac0db353bc092a837bf326a3127db
Misha Ivanov <chesta33@gmail.com>
2023-12-20 12:08:10 +0300

    Update sketch.js

--- p5/moving_circle/sketch.js
+++ p5/moving_circle/sketch.js
@@ -1,17 +1,10 @@
-let x, y;
 let diameter = 30;
-const step = 4;
 const sizeChangeStep = 5;
-let movingLeft = false;
-let movingRight = false;
-let movingUp = false;
-let movingDown = false;
-let trailEnabled = false; // Переменная для отслеживания состояния шлейфа
+let trailEnabled = false;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    x = width / 2;
-    y = height / 2;
+    background(0);
 }
 
 function draw() {
@@ -19,55 +12,27 @@ function draw() {
         background(0); // Очищаем фон, если шлейф отключен
     }
 
-    handleGamepad();
-
-    if (movingLeft) {
-        x -= step;
-    }
-    if (movingRight) {
-        x += step;
-    }
-    if (movingUp) {
-        y -= step;
-    }
-    if (movingDown) {
-        y += step;
-    }
+    handleKeyboard();
 
     fill(255);
-    ellipse(x, y, diameter, diameter);
+    ellipse(mouseX, mouseY, diameter, diameter);
+}
+
+function handleKeyboard() {
+    if (keyIsDown(49)) { // Клавиша "1"
+        diameter = max(10, diameter - sizeChangeStep);
+    }
+    if (keyIsDown(50)) { // Клавиша "2"
+        diameter = min(2000, diameter + sizeChangeStep);
+    }
+}
+
+function keyPressed() {
+    if (keyCode === 32) { // Пробел
+        trailEnabled = !trailEnabled;
+    }
 }
 
 function handleGamepad() {
-    let gamepads = navigator.getGamepads();
-    if (gamepads[0]) {
-        let gp = gamepads[0];
-
-        // Обработка движения
-        let leftStickX = gp.axes[0];
-        let leftStickY = gp.axes[1];
-
-        if (Math.abs(leftStickX) > 0.1) {
-            x += leftStickX * step;
-        }
-        if (Math.abs(leftStickY) > 0.1) {
-            y += leftStickY * step;
-        }
-
-        // Обработка изменения размера
-        let L2 = gp.buttons[6].value;
-        let R2 = gp.buttons[7].value;
-
-        if (L2 > 0.1) {
-            diameter = max(10, diameter - L2 * sizeChangeStep);
-        }
-        if (R2 > 0.1) {
-            diameter = min(2000, diameter + R2 * sizeChangeStep);
-        }
-
-        // Включение/выключение шлейфа
-        if (gp.buttons[0].pressed) {
-            trailEnabled = !trailEnabled;
-        }
-    }
+    // Ваш код для обработки ввода с геймпада
 }
