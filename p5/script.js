82d6e96ff2a3fdcb43f7ee037133cffac6535447
Misha Ivanov <chesta33@gmail.com>
2023-12-20 19:31:09 +0300

    Update script.js

--- p5/script.js
+++ p5/script.js
@@ -1,6 +1,6 @@
-const repoOwner = 'whatmisha'; // Ваше имя пользователя на GitHub
-const repoName = 'mishaivanov.ru';  // Название вашего репозитория
-const path = 'p5'; // Путь к папке в вашем репозитории
+const repoOwner = 'whatmisha'; // имя пользователя на GitHub
+const repoName = 'mishaivanov.ru';  // название репозитория
+const path = 'p5'; // путь к папке в репозитории
 
 fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${path}`)
     .then(response => response.json())
@@ -10,7 +10,6 @@ fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${path}`)
 
         data.forEach(item => {
             if (item.type === 'dir') {
-                // Обновленная ссылка, ведущая на ваш собственный домен
                 const projectUrl = `https://mishaivanov.ru/${path}/${item.name}/`;
 
                 const link = document.createElement('a');
