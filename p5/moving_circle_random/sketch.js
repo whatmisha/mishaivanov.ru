1fdac9b02687484b42e2e3aa11d4c6906fb08d0d
Misha Ivanov <chesta33@gmail.com>
2023-12-20 15:32:44 +0300

    Update sketch.js

--- p5/moving_circle_random/sketch.js
+++ p5/moving_circle_random/sketch.js
@@ -27,7 +27,23 @@ function draw() {
 }
 
 function drawBezierCircle(cx, cy, r) {
-    // Оставляем эту функцию без изменений
+    const handleLength = r * 0.552284749831;
+
+    fill(255);       // Белый цвет заливки
+    stroke(0);       // Чёрный цвет обводки
+    strokeWeight(1); // Толщина обводки в 1 пиксель
+
+    beginShape();
+    // Верхняя правая часть
+    vertex(cx, cy - r);
+    bezierVertex(cx + handleLength, cy - r, cx + r, cy - handleLength, cx + r, cy);
+    // Нижняя правая часть
+    bezierVertex(cx + r, cy + handleLength, cx + handleLength, cy + r, cx, cy + r);
+    // Нижняя левая часть
+    bezierVertex(cx - handleLength, cy + r, cx - r, cy + handleLength, cx - r, cy);
+    // Верхняя левая часть
+    bezierVertex(cx - r, cy - handleLength, cx - handleLength, cy - r, cx, cy - r);
+    endShape(CLOSE);
 }
 
 function drawAngularCircle(cx, cy, r) {
@@ -50,11 +66,37 @@ function handleGamepad() {
     let gamepads = navigator.getGamepads();
     if (gamepads[0]) {
         let gp = gamepads[0];
-        // Обработка движения и изменения размера остаётся без изменений
+
+        // Левый стик контроллера для управления положением
+        let leftStickX = gp.axes[0];
+        let leftStickY = gp.axes[1];
+
+        if (Math.abs(leftStickX) > 0.1) {
+            x += leftStickX * step;
+        }
+        if (Math.abs(leftStickY) > 0.1) {
+            y += leftStickY * step;
+        }
+
+        // Кнопки L2 и R2 для изменения размера
+        let L2 = gp.buttons[6].value;
+        let R2 = gp.buttons[7].value;
+
+        if (L2 > 0.1) {
+            diameter = max(10, diameter - L2 * sizeChangeStep);
+        }
+        if (R2 > 0.1) {
+            diameter = min(2000, diameter + R2 * sizeChangeStep);
+        }
 
         // Кнопка "Квадрат" для переключения стиля контура
         if (gp.buttons[2].pressed) {
             smoothContour = !smoothContour;
+        }
+
+        // Кнопка "Крестик" для включения/выключения шлейфа
+        if (gp.buttons[0].pressed) {
+            trailEnabled = !trailEnabled;
         }
     }
 }
