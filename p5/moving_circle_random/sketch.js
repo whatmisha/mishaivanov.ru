ee14c6840df22716e364f3520614b46fd53afe23
Misha Ivanov <chesta33@gmail.com>
2023-12-20 13:44:20 +0300

    Update sketch.js

--- p5/moving_circle_random/sketch.js
+++ p5/moving_circle_random/sketch.js
@@ -1,7 +1,24 @@
+let x, y;
+let diameter = 30;
+const step = 4; // Скорость перемещения для контроллера
+const sizeChangeStep = 5;
+let trailEnabled = false;
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(windowWidth, windowHeight);
+    x = width / 2;
+    y = height / 2;
     background(0);
-    drawBezierCircle(200, 200, 15); // Центр круга (200, 200) и радиус 15
+}
+
+function draw() {
+    if (!trailEnabled) {
+        background(0); // Очищаем фон, если шлейф отключен
+    }
+
+    handleGamepad();
+
+    drawBezierCircle(x, y, diameter / 2); // Используем функцию для рисования круга
 }
 
 function drawBezierCircle(cx, cy, r) {
@@ -20,4 +37,43 @@ function drawBezierCircle(cx, cy, r) {
     // Верхняя левая часть
     bezierVertex(cx - r, cy - handleLength, cx - handleLength, cy - r, cx, cy - r);
     endShape(CLOSE);
+}
+
+function handleGamepad() {
+    let gamepads = navigator.getGamepads();
+    if (gamepads[0]) {
+        let gp = gamepads[0];
+
+        // Левый стик контроллера
+        let leftStickX = gp.axes[0];
+        let leftStickY = gp.axes[1];
+
+        if (Math.abs(leftStickX) > 0.1) {
+            x += leftStickX * step;
+        }
+        if (Math.abs(leftStickY) > 0.1) {
+            y += leftStickY * step;
+        }
+
+        // Кнопки L2 и R2
+        let L2 = gp.buttons[6].value;
+        let R2 = gp.buttons[7].value;
+
+        if (L2 > 0.1) {
+            diameter = max(10, diameter - L2 * sizeChangeStep);
+        }
+        if (R2 > 0.1) {
+            diameter = min(2000, diameter + R2 * sizeChangeStep);
+        }
+
+        // Кнопка "Крестик"
+        if (gp.buttons[0].pressed) {
+            trailEnabled = !trailEnabled;
+        }
+    }
+}
+
+function mouseMoved() {
+    x = mouseX;
+    y = mouseY;
 }
