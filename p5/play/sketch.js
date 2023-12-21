c49ecff0454c5bab6535a5409f402aed327da94d
Misha Ivanov <chesta33@gmail.com>
2023-12-21 23:31:34 +0300

    Fix

--- p5/play/sketch.js
+++ p5/play/sketch.js
@@ -1,35 +1,46 @@
 let x, y; // Позиция буквы "A"
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  textFont('Angst'); // Используем шрифт Angst, определенный в CSS
+  // Создаем холст и размещаем его в теле документа
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.style('display', 'block'); // Устраняем стандартные отступы
+
+  // Настройки текста
+  textFont('Angst'); // Указываем имя шрифта, определенного в CSS
   textSize(240);
   textAlign(CENTER, CENTER);
-  fill(255); // Белый цвет текста
+  fill(255); // Устанавливаем белый цвет текста
+
+  // Изначально размещаем букву "A" в центре экрана
   x = width / 2;
   y = height / 2;
 
+  // Обработчик событий для подключения геймпада
   window.addEventListener("gamepadconnected", function(e) {
-    console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
-      e.gamepad.index, e.gamepad.id,
-      e.gamepad.buttons.length, e.gamepad.axes.length);
+    console.log(`Gamepad connected at index ${e.gamepad.index}: ${e.gamepad.id}.`);
   });
 }
 
 function draw() {
-  background(0); // Черный фон
-  text('A', x, y); // Отрисовка буквы "A"
+  background(0); // Устанавливаем черный фон
 
-  // Обновление позиции буквы с помощью левого стика Dualshock
+  // Отрисовка буквы "A" в текущей позиции (x, y)
+  text('A', x, y);
+
+  // Попытка получить данные от первого подключенного геймпада
   let gamepads = navigator.getGamepads();
   if (gamepads[0]) {
-    x += gamepads[0].axes[0] * 5; // изменение позиции по оси X
-    y += gamepads[0].axes[1] * 5; // изменение позиции по оси Y
+    let gp = gamepads[0];
+
+    // Обновляем позицию буквы "A" на основе положения левого стика
+    x += gp.axes[0] * 5; // Горизонтальное движение
+    y += gp.axes[1] * 5; // Вертикальное движение
   }
 }
 
+// Функция, вызываемая при изменении размеров окна браузера
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  x = width / 2; // Сброс позиции буквы "A" в центр
+  x = width / 2; // Сбрасываем позицию буквы "A" в центр
   y = height / 2;
 }
