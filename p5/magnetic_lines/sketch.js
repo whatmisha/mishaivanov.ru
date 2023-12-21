c48811cf3889418afbbe8554ae32104b6acf5cb8
Misha Ivanov <chesta33@gmail.com>
2023-12-21 12:13:40 +0300

    Update sketch.js

--- p5/magnetic_lines/sketch.js
+++ p5/magnetic_lines/sketch.js
@@ -1,26 +1,15 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
-  noFill();
 }
 
 function draw() {
-  background(0); // Перерисовываем фон, чтобы курсор оставался одиноким кругом, а не следом.
+  background(0); // Перерисовываем фон, чтобы курсор не оставлял след.
   stroke(255);
   strokeWeight(1);
 
-  // Определяем точки для кривой Безье
-  let startX = 0;
-  let startY = windowHeight;
-  let endX = windowWidth;
-  let endY = 0;
-  let controlX1 = windowWidth / 4;
-  let controlY1 = windowHeight;
-  let controlX2 = 3 * windowWidth / 4;
-  let controlY2 = 0;
-
-  // Рисуем кривую Безье
-  bezier(startX, startY, controlX1, controlY1, controlX2, controlY2, endX, endY);
+  // Рисуем простую диагональную линию слева-направо снизу-вверх
+  line(0, windowHeight, windowWidth, 0);
 
   // Рисуем курсор
   drawCustomCursor();
