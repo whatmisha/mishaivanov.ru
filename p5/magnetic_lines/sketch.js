939b2f1993319f03c9349f3aa26b8b624b842c76
Misha Ivanov <chesta33@gmail.com>
2023-12-21 13:35:35 +0300

    Update sketch.js

--- p5/magnetic_lines/sketch.js
+++ p5/magnetic_lines/sketch.js
@@ -1,73 +1,77 @@
+// Определение переменных для отслеживания положения и движения курсора
 let cursorX;
 let cursorY;
-let sensitivity = 12; // Учетверенная чувствительность для быстрого движения курсора
+let sensitivity = 12; // Чувствительность движения курсора
+
+// Переменные для управления размером и углом наклона линий
 let squareSize = 30; // Начальный размер квадрата
 let baseAngle = 0; // Исходный угол наклона линии
+
+// Переменные для управления цветом
 let invertColors = false; // Флаг для инвертирования цветов
+let buttonPressed = false; // Отслеживание предыдущего состояния кнопки крестика
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight); // Создание холста
   cursor('none'); // Скрываем системный курсор
-  // Устанавливаем начальное положение курсора в центре экрана
-  cursorX = windowWidth / 2;
-  cursorY = windowHeight / 2;
+  cursorX = windowWidth / 2; // Центрируем курсор по горизонтали
+  cursorY = windowHeight / 2; // Центрируем курсор по вертикали
 }
 
 function draw() {
-  // Установка цвета фона в зависимости от инверсии
-  background(invertColors ? 255 : 0);
-  handleGamepad(); // Обрабатываем ввод с контроллера
-  drawGrid(squareSize);
-  drawCustomCursor();
+  background(invertColors ? 255 : 0); // Установка цвета фона в зависимости от флага инверсии
+  handleGamepad(); // Обработка ввода с контроллера
+  drawGrid(squareSize); // Рисование сетки линий
+  drawCustomCursor(); // Рисование кастомного курсора
 }
 
 function handleGamepad() {
-  let gamepads = navigator.getGamepads();
+  let gamepads = navigator.getGamepads(); // Получение списка подключенных контроллеров
   if (gamepads[0]) {
-    let gp = gamepads[0];
+    let gp = gamepads[0]; // Работаем с первым подключенным контроллером
 
-    // Обновляем положение курсора на основе левого стика контроллера
+    // Обновление положения курсора на основе левого стика контроллера
     cursorX += sensitivity * gp.axes[0];
     cursorY += sensitivity * gp.axes[1];
 
-    // Вращаем исходное положение линий с помощью правого стика
+    // Вращение исходного положения линий с помощью правого стика
     let rotationSensitivity = 0.05; // Чувствительность вращения
-    baseAngle += rotationSensitivity * gp.axes[2]; // Добавляем к углу наклона
+    baseAngle += rotationSensitivity * gp.axes[2]; // Изменение угла наклона
 
-    // Изменяем размер квадратов с помощью кнопок L2 и R2
-    if (gp.buttons[6].value > 0) {
-      squareSize += 1;
-    }
-    if (gp.buttons[7].value > 0) {
-      squareSize = max(10, squareSize - 1);
-    }
+    // Изменение размера квадратов с помощью кнопок L2 и R2
+    if (gp.buttons[6].value > 0) squareSize += 1;
+    if (gp.buttons[7].value > 0) squareSize = max(10, squareSize - 1);
 
-    // Инвертируем цвета при нажатии кнопки крестика (gp.buttons[0])
-    if (gp.buttons[0].pressed) {
-      invertColors = !invertColors;
+    // Переключение цветов при однократном нажатии кнопки крестика
+    if (gp.buttons[0].pressed && !buttonPressed) {
+      invertColors = !invertColors; // Инвертирование флага цвета
+      buttonPressed = true; // Обновление состояния кнопки
+    } else if (!gp.buttons[0].pressed) {
+      buttonPressed = false; // Сброс состояния кнопки, если она отпущена
     }
   }
 }
 
 function drawGrid(squareSize) {
-  // Установка цвета линий в зависимости от инверсии
-  stroke(invertColors ? 0 : 255);
-  strokeWeight(1);
+  stroke(invertColors ? 0 : 255); // Установка цвета линий в зависимости от инверсии
+  strokeWeight(1); // Толщина линий
 
+  // Создание сетки из линий
   for (let x = 0; x < windowWidth; x += squareSize) {
     for (let y = 0; y < windowHeight; y += squareSize) {
-      let angle = atan2(cursorY - (y + squareSize / 2), cursorX - (x + squareSize / 2));
+      // Вычисление угла между курсором и центром квадрата
+      let cursorAngle = atan2(cursorY - (y + squareSize / 2), cursorX - (x + squareSize / 2));
       push();
-      translate(x + squareSize / 2, y + squareSize / 2);
-      rotate(baseAngle + angle);
-      line(-squareSize / 2, 0, squareSize / 2, 0);
+      translate(x + squareSize / 2, y + squareSize / 2); // Перемещение начала координат в центр квадрата
+      rotate(baseAngle + cursorAngle); // Применение общего угла вращения
+      line(-squareSize / 2, 0, squareSize / 2, 0); // Рисование линии
       pop();
     }
   }
 }
 
 function drawCustomCursor() {
-  fill(invertColors ? 0 : 255);
-  noStroke();
-  ellipse(cursorX, cursorY, 20, 20);
+  fill(invertColors ? 0 : 255); // Установка цвета курсора в зависимости от инверсии
+  noStroke(); // Без обводки
+  ellipse(cursorX, cursorY, 20, 20); // Рисование эллипса в позиции курсора
 }
