412f47f3d593009b8290236d0635c5a4206b3318
Misha Ivanov <chesta33@gmail.com>
2023-12-21 23:22:58 +0300

    Update sketch.js

--- p5/play/sketch.js
+++ p5/play/sketch.js
@@ -2,14 +2,13 @@ let x, y; // Позиция буквы "A"
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  textFont('Angst'); // Используем шрифт Angst, загруженный через CSS
+  textFont('Angst'); // Используем шрифт Angst, определенный в CSS
   textSize(240);
   textAlign(CENTER, CENTER);
   fill(255); // Белый цвет текста
   x = width / 2;
   y = height / 2;
 
-  // Слушатель для подключения геймпада
   window.addEventListener("gamepadconnected", function(e) {
     console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
       e.gamepad.index, e.gamepad.id,
@@ -19,9 +18,9 @@ function setup() {
 
 function draw() {
   background(0); // Черный фон
-  text('A', x, y); // Отрисовка буквы "A" в текущей позиции (x, y)
+  text('A', x, y); // Отрисовка буквы "A"
 
-  // Получение данных от геймпада
+  // Обновление позиции буквы с помощью левого стика Dualshock
   let gamepads = navigator.getGamepads();
   if (gamepads[0]) {
     x += gamepads[0].axes[0] * 5; // изменение позиции по оси X
@@ -30,7 +29,6 @@ function draw() {
 }
 
 function windowResized() {
-  // Обработка изменения размера окна
   resizeCanvas(windowWidth, windowHeight);
   x = width / 2; // Сброс позиции буквы "A" в центр
   y = height / 2;
