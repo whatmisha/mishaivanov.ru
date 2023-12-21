8856081d1296062c33b4af1915767635a6d9a1f3
Misha Ivanov <chesta33@gmail.com>
2023-12-21 12:49:33 +0300

    Update sketch.js

--- p5/magnetic_lines/sketch.js
+++ p5/magnetic_lines/sketch.js
@@ -1,22 +1,42 @@
+let cursorX;
+let cursorY;
+let sensitivity = 3; // Чувствительность движения курсора
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
   cursor('none'); // Скрываем системный курсор
   // Устанавливаем начальное положение курсора в центре экрана
-  mouseX = windowWidth / 2;
-  mouseY = windowHeight / 2;
+  cursorX = windowWidth / 2;
+  cursorY = windowHeight / 2;
 }
 
 function draw() {
-  background(0); // Перерисовываем фон, чтобы курсор не оставлял след.
+  background(0);
+  handleGamepad(); // Обрабатываем ввод с контроллера
+  drawGrid();
+  drawCustomCursor();
+}
+
+function handleGamepad() {
+  // Получаем данные от первого подключенного контроллера
+  let gamepads = navigator.getGamepads();
+  if (gamepads[0]) {
+    let gp = gamepads[0];
+    // Обновляем положение курсора на основе левого стика контроллера (обычно axes[0] и axes[1])
+    cursorX += sensitivity * gp.axes[0];
+    cursorY += sensitivity * gp.axes[1];
+  }
+}
+
+function drawGrid() {
   stroke(255);
   strokeWeight(1);
-
   let squareSize = 30;
 
   for (let x = 0; x < windowWidth; x += squareSize) {
     for (let y = 0; y < windowHeight; y += squareSize) {
-      let angle = atan2(mouseY - (y + squareSize / 2), mouseX - (x + squareSize / 2));
+      let angle = atan2(cursorY - (y + squareSize / 2), cursorX - (x + squareSize / 2));
       push();
       translate(x + squareSize / 2, y + squareSize / 2);
       rotate(angle);
@@ -24,12 +44,10 @@ function draw() {
       pop();
     }
   }
-
-  drawCustomCursor();
 }
 
 function drawCustomCursor() {
   noStroke();
   fill(255);
-  ellipse(mouseX, mouseY, 20, 20);
+  ellipse(cursorX, cursorY, 20, 20);
 }
