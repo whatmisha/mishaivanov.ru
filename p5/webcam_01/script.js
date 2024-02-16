7a4bb1a6f958b41da001e46b89bbf1eb6d558089
Misha Ivanov <chesta33@gmail.com>
2024-02-16 20:29:53 +0300

    Body tracking

--- p5/webcam_01/script.js
+++ p5/webcam_01/script.js
@@ -1,18 +1,59 @@
-let capture;
+let video;
+let poseNet;
+let poses = [];
 
 function setup() {
-    createCanvas(windowWidth, windowHeight); // Создаем канвас на весь экран
-    capture = createCapture(VIDEO); // Запрашиваем видео с веб-камеры
-    capture.size(windowWidth, windowHeight); // Размер видео - на весь экран
-    capture.hide(); // Скрываем оригинальный HTML элемент видео, чтобы мы могли рисовать его на канвасе p5.js
+    createCanvas(640, 480);
+    video = createCapture(VIDEO);
+    video.size(width, height);
+
+    // Отражаем видео по горизонтали для эффекта зеркала
+    video.style('transform', 'scale(-1, 1)');
+
+    poseNet = ml5.poseNet(video, modelReady);
+    poseNet.on('pose', function(results) {
+        poses = results;
+    });
+    video.hide(); // Скрываем HTML-элемент видео
+}
+
+function modelReady() {
+    console.log('Model Loaded');
 }
 
 function draw() {
-    image(capture, 0, 0, windowWidth, windowHeight); // Отображаем видео на канвасе
+    // Отображаем видео, отраженное по горизонтали
+    translate(width, 0); // Смещаем систему координат к правому краю канваса
+    scale(-1, 1); // Применяем масштабирование для отражения содержимого канваса
+    image(video, 0, 0, width, height);
+    drawKeypoints();
+    drawSkeleton();
 }
 
-// При изменении размера окна браузера обновляем размер канваса и видео
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-    capture.size(windowWidth, windowHeight);
+// Рисуем ключевые точки позы на канвасе
+function drawKeypoints()  {
+    for (let i = 0; i < poses.length; i++) {
+        let pose = poses[i].pose;
+        for (let j = 0; j < pose.keypoints.length; j++) {
+            let keypoint = pose.keypoints[j];
+            if (keypoint.score > 0.2) {
+                fill(255, 0, 0);
+                noStroke();
+                ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+            }
+        }
+    }
+}
+
+// Добавим функцию для рисования скелета
+function drawSkeleton() {
+    for (let i = 0; i < poses.length; i++) {
+        let skeleton = poses[i].skeleton;
+        for (let j = 0; j < skeleton.length; j++) {
+            let partA = skeleton[j][0];
+            let partB = skeleton[j][1];
+            stroke(255, 0, 0);
+            line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+        }
+    }
 }
