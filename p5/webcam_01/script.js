485b83a675e1cff48a395c8c41624de24c4440a8
Misha Ivanov <chesta33@gmail.com>
2024-02-17 17:37:07 +0300

    Update script.js

--- p5/webcam_01/script.js
+++ p5/webcam_01/script.js
@@ -4,14 +4,15 @@ let poses = [];
 
 function setup() {
     createCanvas(windowWidth, windowHeight); // Создаем холст размером с окно браузера
-    video = createCapture(VIDEO);
-    video.size(width, height);
+    video = createCapture(VIDEO); // Захватываем видео с камеры
+    video.size(width, height); // Задаем размер видео равный размеру холста
+    video.hide(); // Скрываем HTML-элемент видео, чтобы отобразить его на холсте
     
+    // Инициализация PoseNet
     poseNet = ml5.poseNet(video, modelReady);
     poseNet.on('pose', function(results) {
         poses = results;
     });
-    video.hide(); // Скрываем HTML-элемент видео
 }
 
 function modelReady() {
@@ -19,35 +20,38 @@ function modelReady() {
 }
 
 function draw() {
-    background(0);
-    image(video, 0, 0, width, height);
-    drawKeypoints();
-    drawSkeleton();
+    background(0); // Задаем черный фон
+    image(video, 0, 0, width, height); // Отображаем видео на весь холст
+    
+    drawKeypoints(); // Рисуем ключевые точки
+    drawSkeleton(); // Рисуем скелет
 }
 
+// Функция для отрисовки ключевых точек
 function drawKeypoints() {
     for (let i = 0; i < poses.length; i++) {
         let pose = poses[i].pose;
         for (let j = 0; j < pose.keypoints.length; j++) {
             let keypoint = pose.keypoints[j];
             if (keypoint.score > 0.2) {
-                fill(0, 0, 255); // Синий цвет
+                fill(0, 0, 255); // Задаем цвет точек
                 noStroke();
-                ellipse(keypoint.position.x, keypoint.position.y, 40, 40); // Размер маркера
+                ellipse(keypoint.position.x, keypoint.position.y, 40, 40); // Рисуем точки на ключевых точках
             }
         }
     }
 }
 
+// Функция для отрисовки скелета
 function drawSkeleton() {
     for (let i = 0; i < poses.length; i++) {
         let skeleton = poses[i].skeleton;
         for (let j = 0; j < skeleton.length; j++) {
             let partA = skeleton[j][0];
             let partB = skeleton[j][1];
-            stroke(0, 0, 255);
+            stroke(0, 0, 255); // Задаем цвет линий
             strokeWeight(40);
-            line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+            line(partA.position.x, partA.position.y, partB.position.x, partB.position.y); // Рисуем линии между ключевыми точками
         }
     }
 }
