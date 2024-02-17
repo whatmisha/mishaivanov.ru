db7f75e05dc211ff490012ea23c6083fba4be0d5
Misha Ivanov <chesta33@gmail.com>
2024-02-17 18:16:53 +0300

    Grayscale

--- p5/webcam_03/script.js
+++ p5/webcam_03/script.js
@@ -4,47 +4,34 @@ const ctx = canvas.getContext('2d');
 
 async function setupCamera() {
     const stream = await navigator.mediaDevices.getUserMedia({
-        'audio': false,
-        'video': {
-            facingMode: 'user',
-        },
+        audio: false,
+        video: {facingMode: 'user'},
     });
     video.srcObject = stream;
-
     return new Promise((resolve) => {
-        video.onloadedmetadata = () => {
-            resolve(video);
-        };
+        video.onloadedmetadata = () => resolve(video);
     });
 }
 
 async function loadHandpose() {
-    const net = await handpose.load();
-    console.log('Handpose model loaded');
-    return net;
+    return await handpose.load();
 }
 
 async function detect(net) {
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
-
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     const predictions = await net.estimateHands(video);
     
-    if (predictions.length > 0) {
-        for (let i = 0; i < predictions.length; i++) {
-            const landmarks = predictions[i].landmarks;
-
-            // Draw landmarks
-            for (let j = 0; j < landmarks.length; j++) {
-                const [x, y, z] = landmarks[j];
-                ctx.beginPath();
-                ctx.arc(x, y, 5, 0, 3 * Math.PI);
-                ctx.fillStyle = 'aqua';
-                ctx.fill();
-            }
-        }
-    }
+    predictions.forEach(prediction => {
+        const landmarks = prediction.landmarks;
+        landmarks.forEach(([x, y]) => {
+            ctx.beginPath();
+            ctx.arc(x, y, 5, 0, 2 * Math.PI);
+            ctx.fillStyle = 'blue';
+            ctx.fill();
+        });
+    });
 
     requestAnimationFrame(() => detect(net));
 }
