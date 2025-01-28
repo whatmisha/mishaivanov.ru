e3847bd32371ea71abf7671e8268ad303f3bd5ac
Misha Ivanov <chesta33@gmail.com>
2025-01-28 12:14:36 +0100

    -

---
+++ js/ony/ony_track/tracklist/script.js
@@ -0,0 +1,11 @@
+// Подключаем p5.js через CDN
+<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.0/p5.js"></script>
+
+// Базовые функции p5.js для начала работы
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
