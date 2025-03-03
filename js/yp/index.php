f40d81ba9d26bc81d74767021b698818a6ca2c7f
Misha Ivanov <chesta33@gmail.com>
2025-03-03 11:04:11 +0100

    -

--- js/yp/index.php
+++ js/yp/index.php
@@ -8,6 +8,7 @@ function getLatestVersion() {
     $versions = [];
     foreach ($dirs as $dir) {
         $dirName = basename($dir);
+        // Изменяем регулярное выражение, чтобы оно принимало любое количество цифр
         if (preg_match('/^\d+$/', $dirName)) {
             $versions[] = $dirName;
         }
@@ -15,16 +16,20 @@ function getLatestVersion() {
     
     // Если версий нет, возвращаем дефолтное значение
     if (empty($versions)) {
-        return "10"; // Дефолтная версия
+        return "00"; // Дефолтная версия
     }
     
     // Сортируем версии по числовому значению
     usort($versions, function($a, $b) {
+        // Преобразуем строки в числа для корректного сравнения
         return intval($a) - intval($b);
     });
     
-    // Возвращаем последнюю версию
-    return end($versions);
+    // Получаем последнюю версию
+    $latestVersion = end($versions);
+    
+    // Форматируем версию, чтобы она имела как минимум 2 цифры
+    return str_pad($latestVersion, 2, '0', STR_PAD_LEFT);
 }
 
 // Получаем последнюю версию
@@ -32,11 +37,20 @@ $latestVersion = getLatestVersion();
 
 // Если запрос идет на конкретную версию, проверяем ее существование
 $requestedVersion = isset($_GET['version']) ? $_GET['version'] : null;
-if ($requestedVersion && is_dir('./' . $requestedVersion)) {
-    // Если запрошенная версия существует, используем ее
-    $version = $requestedVersion;
+if ($requestedVersion !== null) {
+    // Проверяем, что запрошенная версия содержит только цифры
+    if (preg_match('/^\d+$/', $requestedVersion)) {
+        // Форматируем запрошенную версию
+        $requestedVersion = str_pad($requestedVersion, 2, '0', STR_PAD_LEFT);
+        if (is_dir('./' . $requestedVersion)) {
+            $version = $requestedVersion;
+        } else {
+            $version = $latestVersion;
+        }
+    } else {
+        $version = $latestVersion;
+    }
 } else {
-    // Иначе используем последнюю версию
     $version = $latestVersion;
 }
 
