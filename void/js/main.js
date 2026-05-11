6f659990128cada4b9d00e02c152cb11684bb9db
Misha Ivanov <66462326+whatmisha@users.noreply.github.com>
2026-05-11 21:52:03 +0200

    Update main.js

--- void/js/main.js
+++ void/js/main.js
@@ -48,7 +48,7 @@ class VoidTypeface {
                 letterColor: '#ffffff',
                 bgColor: '#000000',
                 gridColor: '#333333',
-                text: 'Void\nTypeface\nCode',
+                text: 'Void\nTypeface\Tool',
                 textAlign: 'center',
                 showGrid: true,
                 showJoints: false,
